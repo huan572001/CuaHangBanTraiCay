6bffb58f7070535c460ee7baf87ab64567ebcd94
huan572001 <85747713+huan572001@users.noreply.github.com>
2023-04-17 01:30:05 +0700

    them man hinh otp va dang ky

--- app/src/main/java/com/example/cuahangbantraicay/activity/DangKy.java
+++ app/src/main/java/com/example/cuahangbantraicay/activity/DangKy.java
@@ -4,8 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.example.cuahangbantraicay.R;
@@ -13,9 +15,15 @@ import com.example.cuahangbantraicay.R;
 public class DangKy extends AppCompatActivity {
     TextView DN;
     Button btnDK;
+    EditText edt_fullname,edt_email,edt_phone,edt_password,edt_confirm_password;
     private  void setControl() {
         DN=findViewById(R.id.linkDN);
         btnDK=findViewById(R.id.btnDK);
+        edt_fullname=findViewById(R.id.edt_fullName);
+        edt_email=findViewById(R.id.edt_emailDK);
+        edt_phone=findViewById(R.id.edt_SDT);
+        edt_password=findViewById(R.id.edt_MKDK);
+        edt_confirm_password=findViewById(R.id.edt_XNMK);
     }
     private void setEvent(){
         DN.setOnClickListener(new View.OnClickListener() {
@@ -28,8 +36,30 @@ public class DangKy extends AppCompatActivity {
         btnDK.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(DangKy.this, OTP.class);
-                startActivity(intent);
+
+                if(TextUtils.isEmpty(edt_fullname.getText().toString())){
+                    edt_fullname.setError("Không được để trống!");
+                    edt_fullname.requestFocus();
+                } else if (TextUtils.isEmpty(edt_email.getText().toString())) {
+                    edt_email.setError("Không được để trống!");
+                    edt_email.requestFocus();
+                }else if (TextUtils.isEmpty(edt_phone.getText().toString())) {
+                    edt_phone.setError("Không được để trống!");
+                    edt_phone.requestFocus();
+                }else if (TextUtils.isEmpty(edt_password.getText().toString())) {
+                    edt_password.setError("Không được để trống!");
+                    edt_password.requestFocus();
+                }else if (TextUtils.isEmpty(edt_confirm_password.getText().toString())) {
+                    edt_confirm_password.setError("Không được để trống!");
+                    edt_confirm_password.requestFocus();
+                }else if (!edt_password.getText().toString().equals(edt_confirm_password.getText().toString())) {
+                    edt_confirm_password.setError("Mật khẩu xác nhận không khớp");
+                    edt_confirm_password.requestFocus();
+                }else {
+                    Intent intent = new Intent(DangKy.this, OTP.class);
+                    startActivity(intent);
+                }
+
             }
         });
     }
