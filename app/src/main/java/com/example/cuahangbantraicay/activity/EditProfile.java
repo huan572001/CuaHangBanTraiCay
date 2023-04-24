c3236624b800d1031be234fe95b0e4bff5625249
dblue1st <zdbluez@gmail.com>
2023-04-24 21:35:28 +0700

    update EditProfile

--- app/src/main/java/com/example/cuahangbantraicay/activity/EditProfile.java
+++ app/src/main/java/com/example/cuahangbantraicay/activity/EditProfile.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,14 +17,19 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.example.cuahangbantraicay.API.UserAPI;
 import com.example.cuahangbantraicay.R;
+import com.example.cuahangbantraicay.Utils.VolleyCallback;
 
 import org.jetbrains.annotations.Nullable;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class EditProfile extends AppCompatActivity {
+    int idUser;
     Button btnSaveEdit;
     TextView user_fname, user_phone, user_birthday, user_email,tv_kq;
     CheckBox user_male, user_female;
@@ -41,12 +47,19 @@ public class EditProfile extends AppCompatActivity {
         tv_kq = findViewById(R.id.tv_kq);
     }
 
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.edit_profile);
-        setControl();
+    public void setEvent(){
+        user_male.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(user_male.isChecked()) user_female.setChecked(false);
+            }
+        });
+        user_female.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(user_female.isChecked()) user_male.setChecked(false);
+            }
+        });
         btnSaveEdit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -56,7 +69,7 @@ public class EditProfile extends AppCompatActivity {
 //                startActivity(intent);
                 RequestQueue requestQueue = Volley.newRequestQueue(EditProfile.this);
                 String url = "http://localhost:3000/api/v1/admin/updateUserById/:id";
-                StringRequest stringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+                StringRequest stringRequest = new StringRequest(Request.Method.PUT, url, new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
                         tv_kq.setText("Thay đổi thành công"+response);
@@ -85,5 +98,51 @@ public class EditProfile extends AppCompatActivity {
                 requestQueue.add(stringRequest);
             }
         });
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.edit_profile);
+        setControl();
+        setEvent();
+        getUser();
+
+    }
+    private void getUser() {
+        try {
+            UserAPI.getUser(this, new VolleyCallback() {
+                @Override
+                public void onSuccess(JSONObject result) {
+                    try {
+                        if ((Boolean) result.get("success")) {
+                            JSONObject data = result.getJSONObject("data");
+                            name = data.getString("name");
+                            gender = data.getInt("gender");
+                            email = data.getString("email");
+                            phone = data.getString("phone");
+                            birthday = data.getString("birthday");
+
+                            user_fname.setText(name);
+                            if (gender == 1) {
+                                user_male.setChecked(true);
+                            } else user_female.setChecked(true);
+                            user_email.setText(email);
+                            user_phone.setText(phone);
+                            user_birthday.setText(birthday);
+                        }
+                    } catch (JSONException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+
+                @Override
+                public void onError(JSONObject errorMessage) {
+
+                }
+            }, idUser);
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
